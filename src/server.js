e3bfa52c2d12191973d3bfa7370b5276b6b7c736
hify13 <hify9535@gmail.com>
2025-11-06 13:40:12 -0600

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,61 @@
+import { createServer } from "node:http";
+import { join } from "node:path";
+import { hostname } from "node:os";
+import wisp from "wisp-server-node";
+import Fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+
+// static paths
+import { publicPath } from "ultraviolet-static";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
+
+const fastify = Fastify({
+	serverFactory: (handler) => {
+		return createServer()
+			.on("request", (req, res) => {
+				res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+				res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+				handler(req, res);
+			})
+			.on("upgrade", (req, socket, head) => {
+				if (req.url.endsWith("/wisp/")) wisp.routeRequest(req, socket, head);
+				else socket.end();
+			});
+	},
+});
+
+// static routes
+fastify.register(fastifyStatic, {
+	root: publicPath,
+	decorateReply: true,
+});
+
+fastify.get("/uv/uv.config.js", (req, res) => {
+	return res.sendFile("uv/uv.config.js", publicPath);
+});
+
+fastify.register(fastifyStatic, {
+	root: uvPath,
+	prefix: "/uv/",
+	decorateReply: false,
+});
+
+fastify.register(fastifyStatic, {
+	root: epoxyPath,
+	prefix: "/epoxy/",
+	decorateReply: false,
+});
+
+fastify.register(fastifyStatic, {
+	root: baremuxPath,
+	prefix: "/baremux/",
+	decorateReply: false,
+});
+
+// Export the handler Vercel expects
+export default async function handler(req, res) {
+	await fastify.ready();
+	fastify.server.emit("request", req, res);
+}
